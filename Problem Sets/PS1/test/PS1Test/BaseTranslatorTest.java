40bb521027c7fd6af9f4988a8472a7a2e2c87643
Michael M <spireflux@gmail.com>
2013-03-31 20:20:31 -0400

    Implemented additional bad input tests.  For negative numbers or for numbers larger then the base they are representing.

--- Problem Sets/PS1/test/PS1Test/BaseTranslatorTest.java
+++ Problem Sets/PS1/test/PS1Test/BaseTranslatorTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class BaseTranslatorTest {
+
     @Test
     public void basicBaseTranslatorTest() {
         // Expect that .01 in base-2 is .25 in base-10
@@ -13,31 +14,27 @@ public class BaseTranslatorTest {
         int[] input = {0, 1};
         int[] expectedOutput = {2, 5};
         assertArrayEquals(expectedOutput,
-                          BaseTranslator.convertBase(input, 2, 10, 2));
+                BaseTranslator.convertBase(input, 2, 10, 2));
     }
 
-    // TODO: Write more tests (Problem 2.a)
-    //  If digits[i] < 0 or digits[i] >= baseA for any i, return null
-    //  If baseA < 2, baseB < 2, or precisionB < 1, return null
-    
-    
     @Test
-    public void improperInputTests(){
-// convertBase(int[] digits, int baseA, int baseB, int precisionB)
-        int[] input = {0,1};
+    public void improperInputTests() {
+    // convertBase(int[] digits, int baseA, int baseB, int precisionB)
+        int[] input = {0, 1};
+        int[] badDigits = {0,1,-2,1};
+        int[] badBase = {1,1,5,2};
 
-      // baseA < 2 return null 
-      assertNull(BaseTranslator.convertBase(input,1,10,2));
-      
-      // baseB < 2 return null
-      assertNull(BaseTranslator.convertBase(input,2,1,2));
+        // If baseA < 2, baseB < 2, or precisionB < 1, return null
+        assertNull(BaseTranslator.convertBase(input, 1, 10, 2));
+        assertNull(BaseTranslator.convertBase(input, 2, 1, 2));
+        assertNull(BaseTranslator.convertBase(input, 2, 2, 0));
 
-      // precisionB <2 return null
-      assertNull(BaseTranslator.convertBase(input,2,2,0));      
-      
-      
+        // If digits[i] < 0 or digits[i] >= baseA for any i, return null
+        assertNull(BaseTranslator.convertBase(badDigits, 2, 10, 5));
+        
+        // digits greater then base
+        assertNull(BaseTranslator.convertBase(badBase, 2, 2, 2));
     }
-
-
-
+    
+    
 }
