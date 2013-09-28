38e3ee3ac9be5b4d5dfc7efcb6a5fd006d9418f7
Michael M <spireflux@gmail.com>
2013-09-27 20:52:17 -0400

    Updating Tests

--- Problem Sets/PS1/test/PS1Test/DigitsToStringConverterTest.java
+++ Problem Sets/PS1/test/PS1Test/DigitsToStringConverterTest.java
@@ -20,36 +20,42 @@ public class DigitsToStringConverterTest {
                              input, 4, alphabet));
     }
 
-    
-    
-     /* If digits[i] >= base or digits[i] < 0 for any i, consider the input
-     * invalid, and return null.
-     
-     * If alphabet.length != base, consider the input invalid, and return null.
-     */
-    
-//    public static String convertDigitsToString(int[] digits, int base, char[] alphabet) {
-    
-    
+       
     @Test
     public void badInputTest(){
-        int[] input = {};
-        char[] alphabet = {};
+        int[] input = {0,1,2};
+        char[] alphabet = {'d','c','b','a'};
         String expected = "dcba";
+            
+        // digits[i] >= base    
+        assertNull(DigitsToStringConverter.convertDigitsToString(input, 2, alphabet));
         
-        assertNull(DigitsToStringConverter.convertDigitsToString(input, 4, alphabet));
+        // digits[i] == base
+        assertNull(DigitsToStringConverter.convertDigitsToString(input, 3, alphabet));        
         
-        
-        
+        // alphabet.lenght != base  (Alphabet.length = 4)   
+        assertNull(DigitsToStringConverter.convertDigitsToString(input, 5, alphabet));
     }
      
      
      
+    @Test
+    public void bigTest(){
+        int[] input = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
+                       17, 18, 19, 20, 21, 22, 23, 24, 25};
+        char[] alphabet =
+        {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+         'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+        String expectedOutput = "abcdefghijklmnopqrstuvwxyz";
+
+        assertEquals(expectedOutput,
+                     DigitsToStringConverter.convertDigitsToString(
+                             input, 4, alphabet));
+    }
     
     
     
     
     
-    
-    // TODO: Write more tests (Problem 3.a)
+ 
 }
