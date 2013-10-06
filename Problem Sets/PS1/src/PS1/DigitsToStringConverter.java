7eb4df9cc20501fe7caac99efdd022118abe4325
Michael M <spireflux@gmail.com>
2013-10-06 17:29:33 -0400

    Digits to string conversion work.  Getting wonky errors.

--- Problem Sets/PS1/src/PS1/DigitsToStringConverter.java
+++ Problem Sets/PS1/src/PS1/DigitsToStringConverter.java
@@ -7,9 +7,12 @@ public class DigitsToStringConverter {
      * chars, convert the list of digits into a character string by applying the
      * mapping to each digit in the input.
      *
-     * If digits[i] >= base or digits[i] < 0 for any i, consider the input
-     * invalid, and return null. If alphabet.length != base, consider the input
-     * invalid, and return null.
+     * If digits[i] >= base or digits[i] < 0 
+     * for any i, consider the input invalid, 
+     *   and return null. 
+     * 
+     * If alphabet.length != base, consider the input invalid, 
+     *   and return null.
      *
      * @param digits A list of digits to encode. This object is not mutated.
      * @param base The base the digits are encoded in.
@@ -17,15 +20,20 @@ public class DigitsToStringConverter {
      * mutated.
      * @return A String encoding the input digits with alphabet.
      */
-    public static String convertDigitsToString(int[] digits, int base,
-            char[] alphabet) {
+    public static String convertDigitsToString(int[] digits, int base, char[] alphabet) {
+    
         String ConvertedDigetsOutput = "";
 
         if (alphabet.length != base) {
             return null;
-        } else {
+        } 
+        
+        if (digits.length != alphabet.length){
+            return null;
+        }
+        
             for (int i = 0; i < base; i++) {
-                if (digits[i] < 0) {
+                if (digits[i] < 0 || digits[i] >=base ) {
                     return null;
                 } else {
                     ConvertedDigetsOutput = ConvertedDigetsOutput + alphabet[i];
@@ -33,7 +41,9 @@ public class DigitsToStringConverter {
             }
 
             return ConvertedDigetsOutput;
-        }
+        
 
+        
+        
     }
 }
--- Problem Sets/PS1/test/PS1Test/DigitsToStringConverterTest.java
+++ Problem Sets/PS1/test/PS1Test/DigitsToStringConverterTest.java
@@ -8,6 +8,34 @@ import org.junit.Test;
 public class DigitsToStringConverterTest {
 
     @Test
+    public void badInputTest() {
+        int[] input  = {0, 1, 2, 4};
+        int[] inFive = {0, 1, 2, 4, 5};
+        int[] negInput = {0, 1, -13, 3};
+        char[] alphabet = {'d', 'c', 'b', 'a'};
+        char[] badAlphabet = {'d', 'c', 'b', 'a', 'q', 'w'};
+        //String expected = "dcba";
+
+        // digits[i] >= base    
+        assertNull(DigitsToStringConverter.convertDigitsToString(input, 2, alphabet));
+
+        // digits[i] == base
+        assertNull(DigitsToStringConverter.convertDigitsToString(input, 3, alphabet));
+
+        // alphabet.lenght != base  (Alphabet.length = 4)   
+        assertNull(DigitsToStringConverter.convertDigitsToString(input, 5, alphabet));
+
+        // digits[i] < 0
+        assertNull(DigitsToStringConverter.convertDigitsToString(negInput, 4, alphabet));
+    
+        // digits.length != alphabet.length
+        assertNull(DigitsToStringConverter.convertDigitsToString(inFive,5, badAlphabet));
+    
+        // digits[i] >= base (return null)
+    
+    }
+
+    @Test
     public void basicNumberSerializerTest() {
         // Input is a 4 digit number, 0.123 represented in base 4
         int[] input = {0, 1, 2, 3};
@@ -21,26 +49,9 @@ public class DigitsToStringConverterTest {
                 input, 4, alphabet));
     }
 
-    @Test
-    public void badInputTest() {
-        int[] input = {0, 1, 2};
-        int[] neg_input = {0, 1, -13, 3};
-        char[] alphabet = {'d', 'c', 'b', 'a'};
-        String expected = "dcba";
-
-        // digits[i] >= base    
-        assertNull(DigitsToStringConverter.convertDigitsToString(input, 2, alphabet));
-
-        // digits[i] == base
-        assertNull(DigitsToStringConverter.convertDigitsToString(input, 3, alphabet));
-
-        // alphabet.lenght != base  (Alphabet.length = 4)   
-        assertNull(DigitsToStringConverter.convertDigitsToString(input, 5, alphabet));
-
-        // digits[i] < 0
-        assertNull(DigitsToStringConverter.convertDigitsToString(neg_input, 4, alphabet));
-    }
-
+    
+    
+    
     @Test
     public void bigTest() {
         int[] input = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
@@ -53,4 +64,37 @@ public class DigitsToStringConverterTest {
                 DigitsToStringConverter.convertDigitsToString(
                 input, 26, alphabet));
     }
+
+    
+    // TODO Implement test for reverse alphabet
+    //
+    @Test
+    public void reverseTest() {
+        int[] input = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
+            17, 18, 19, 20, 21, 22, 23, 24, 25};
+        char[] alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+            'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+        String expectedOutput = "abcdefghijklmnopqrstuvwxyz";
+
+        assertEquals(expectedOutput,
+                DigitsToStringConverter.convertDigitsToString(
+                input, 26, alphabet));
+    }
+    
+    @Test
+    public void randomTest() {
+        int[] input = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
+            17, 18, 19, 20, 21, 22, 23, 24, 25};
+        char[] alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+            'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+        String expectedOutput = "abcdefghijklmnopqrstuvwxyz";
+
+        assertEquals(expectedOutput,
+                DigitsToStringConverter.convertDigitsToString(
+                input, 26, alphabet));
+    }
+    
+    
+    
+    
 }
