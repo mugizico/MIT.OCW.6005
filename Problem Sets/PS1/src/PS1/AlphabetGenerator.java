4732b196eba98db111e838de465cd4da520bdad5
Michael M <spireflux@gmail.com>
2013-10-06 10:50:06 -0400

    Cleanup and formatting.

--- Problem Sets/PS1/src/PS1/AlphabetGenerator.java
+++ Problem Sets/PS1/src/PS1/AlphabetGenerator.java
@@ -3,109 +3,97 @@ package PS1;
 import java.util.*;
 
 public class AlphabetGenerator {
+
     /**
      * Given a numeric base, return a char[] that maps every digit that is
      * representable in that base to a lower-case char.
-     * 
+     *
      * This method will try to weight each character of the alphabet
      * proportional to their occurrence in words in a training set.
-     * 
-     * This method should do the following to generate an alphabet:
-     *   1. Count the occurrence of each character a-z in trainingData.
-     *   2. Compute the probability of each character a-z by taking
-     *      (occurrence / total_num_characters).
-     *   3. The output generated in step (2) is a Probability Density Function of the characters in the
-     *      training set. Convert this PDF into a Cumulative Distribution Function for each character.
-     *   4. Multiply the CDF value of each character by the base we are
-     *      converting into.
-     *   5. For each index 0 <= i < base,
-   *      output[i] = (the first character whose CDF * base is > i)
-     * 
-     * A concrete example:
-     *   0. Input = {"aaaaa..." (302 "a"s), "bbbbb..." (500 "b"s),
-     *               "ccccc..." (198 "c"s)}, base = 93
-     *   1. Count(a) = 302, Count(b) = 500, Count(c) = 193
-     *   2. Pr(a) = 302 / 1000 = .302, Pr(b) = 500 / 1000 = .5,  Pr(c) = 198 / 1000 = .198
-     *   3. CDF(a) = .302, CDF(b) = .802, CDF(c) = 1
-     *   4. CDF(a) * base = 28.086, CDF(b) * base = 74.586, CDF(c) * base = 93
-     *   5. Output = {"a", "a", ... (28 As, indexes 0-27),
-     *                "b", "b", ... (47 Bs, indexes 28-74),
-     *                "c", "c", ... (18 Cs, indexes 75-92)}
-     * 
+     *
+     * This method should do the following to generate an alphabet: 1. Count the
+     * occurrence of each character a-z in trainingData. 2. Compute the
+     * probability of each character a-z by taking (occurrence /
+     * total_num_characters). 3. The output generated in step (2) is a
+     * Probability Density Function of the characters in the training set.
+     * Convert this PDF into a Cumulative Distribution Function for each
+     * character. 4. Multiply the CDF value of each character by the base we are
+     * converting into. 5. For each index 0 <= i < base, output[i] = (the first
+     * character whose CDF * base is > i)
+     *
+     * A concrete example: 0. Input = {"aaaaa..." (302 "a"s), "bbbbb..." (500
+     * "b"s), "ccccc..." (198 "c"s)}, base = 93 1. Count(a) = 302, Count(b) =
+     * 500, Count(c) = 193 2. Pr(a) = 302 / 1000 = .302, Pr(b) = 500 / 1000 =
+     * .5, Pr(c) = 198 / 1000 = .198 3. CDF(a) = .302, CDF(b) = .802, CDF(c) = 1
+     * 4. CDF(a) * base = 28.086, CDF(b) * base = 74.586, CDF(c) * base = 93 5.
+     * Output = {"a", "a", ... (28 As, indexes 0-27), "b", "b", ... (47 Bs,
+     * indexes 28-74), "c", "c", ... (18 Cs, indexes 75-92)}
+     *
      * The letters should occur in lexicographically ascending order in the
-     * returned array.
-     *   - {"a", "b", "c", "c", "d"} is a valid output.
-     *   - {"b", "c", "c", "d", "a"} is not.
-     *   
+     * returned array. - {"a", "b", "c", "c", "d"} is a valid output. - {"b",
+     * "c", "c", "d", "a"} is not.
+     *
      * If base >= 0, the returned array should have length equal to the size of
      * the base.
-     * 
+     *
      * If base < 0, return null.
-     * 
+     *
      * If a String of trainingData has any characters outside the range a-z,
      * ignore those characters and continue.
-     * 
+     *
      * @param base A numeric base to get an alphabet for.
      * @param trainingData The training data from which to generate frequency
-     *                     counts. This array is not mutated.
+     * counts. This array is not mutated.
      * @return A char[] that maps every digit of the base to a char that the
-     *         digit should be translated into.
+     * digit should be translated into.
      */
     public static char[] generateFrequencyAlphabet(int base,
-                                                   String[] trainingData) {
+            String[] trainingData) {
         char[] generatedAlphabet = null;
         int totalCount = 0;
         Map<Character, Integer> count = new TreeMap<Character, Integer>();
-        Double[] CDF = null;
-        
-        if (base < 0 ){
-            return null;
-        }else {
-            
-            // Break training data into words
-            for (String words : trainingData){
-            
-                //Break into letters/characters
-                char[] letters = words.toCharArray();
-                
-                 for (char letter : letters) {
-         
-                  Integer frequency = count.get(letter);
-                  count.put(letter, (frequency == null) ? 1 : frequency +1);
-                }
-               
+        Double[] PDF = null;
 
-        
-            }
-  
-            //System.out.println(count.size() + " distinct letters");
-            
-            //System.out.println(count.values());
-            
-            // Trying the array approach
-            CDF = new Double[count.size()]; 
-            totalCount = count.size();
-            int i = 0;
-            // getting all the values out of the map
-            //for (Integer value : count.values()){
-            //    CDF = (double) value /  (double) totalCount;
-            //}
-            
-            Iterator<Integer> itr = count.values().iterator();
-            while (itr.hasNext()){
-                //System.out.println();
-              CDF[i] = (double) itr.next() / (double) totalCount;
-              System.out.println(CDF[i]);
-              i++;
-            }
-            
-            System.out.println(CDF.length);
-            
-            
-            
+        if (base < 0) {
+            return null;
         }
-        
-        
+
+        // Break training data into words
+        for (String words : trainingData) {
+
+            //Break into letters/characters
+            char[] letters = words.toCharArray();
+
+            for (char letter : letters) {
+
+                // Insert some handling for is character, and all lower case?
+                // letter.isCharacter?
+                // charcter.toLowerCase();   
+
+                Integer frequency = count.get(letter);
+                count.put(letter, (frequency == null) ? 1 : frequency + 1);
+            }
+
+        }
+
+        //System.out.println(count.size() + " distinct letters");
+        //System.out.println(count.values());
+
+        // Trying the array approach
+        PDF = new Double[count.size()];
+        totalCount = count.size();
+        int i = 0;
+
+        Iterator<Integer> itr = count.values().iterator();
+        while (itr.hasNext()) {
+            PDF[i] = (double) itr.next() / (double) totalCount;
+            System.out.println(PDF[i]);
+            i++;
+        }
+
+        System.out.println(PDF.length);
+
+
         return generatedAlphabet;
     }
 }
